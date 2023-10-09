334e7c389c8deea2e7a89f817336a309146cda31
Luis Javier Cases <l.cases@cathedralsw.com>
2023-10-09 19:48:17 +0200

    feat(store): use actions type in reducers

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,33 +1,40 @@
-import actions from "./actions";
+import {
+  SET_ADS,
+  START_LOADING,
+  STOP_LOADING,
+  SET_NEW_ADS,
+  REMOVE_AD,
+  SET_NEW_TITLE
+} from "./actions";
 
 export default (state = {}, action = { type: "" }) => {
   switch (action.type) {
-    case actions.setAds().type:
+    case SET_ADS:
       return {
         ...state,
         ads: action.payload
       };
-    case actions.startLoading().type:
+    case START_LOADING:
       return {
         ...state,
         isLoading: true
       };
-    case actions.stopLoading().type:
+    case STOP_LOADING:
       return {
         ...state,
         isLoading: false
       };
-    case actions.createNewAd().type:
+    case SET_NEW_ADS:
       return {
         ...state,
         ads: [...state.ads, action.payload]
       };
-    case actions.removeAd().type:
+    case REMOVE_AD:
       return {
         ...state,
         ads: [...state.ads.filter((ad) => ad.id !== action.payload)]
       };
-    case actions.setNewAdTitle().type:
+    case SET_NEW_TITLE:
       return {
         ...state,
         newAdTitle: action.payload
